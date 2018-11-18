c13c32755226d0146fa7ece828f03bb195008c49
Affan-Ahmed <affan36@outlook.com>
2018-11-17 19:02:59 -0500

    removed caesar.py and made it into one python file, pycrypto.py

--- pycrypto.py
+++ pycrypto.py
@@ -1,1 +1,15 @@
 # Main PyCrypto Class
+
+def caesar_shift(message, key, mode = 'ENCRYPT', custom_alphabet = 'abcdefghijklmnopqrstuvwxyz'):
+    #key *= -1
+    final_message = ''
+    default_alphabet = 'abcdefghijklmnopqrstuvwyz'
+    for char in message:
+        if char.isalpha():
+            if(char.islower()):
+                final_message += custom_alphabet[(default_alphabet.index(char) + key) % len(custom_alphabet)]
+            elif(char.isupper()):
+                final_message += custom_alphabet[(default_alphabet.index(char.lower()) + key) % len(custom_alphabet)].upper()
+        else:
+            final_message += char
+    return final_message
