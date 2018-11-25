6973ebe50d2a51b66cdbc689c639a43a43e94391
Affan-Ahmed <affan36@outlook.com>
2018-11-24 21:32:59 -0500

    ya

--- pycrypto.py
+++ pycrypto.py
@@ -1,15 +1,21 @@
 # Main PyCrypto Class
 
-def caesar_shift(message, key, mode = 'ENCRYPT', custom_alphabet = 'abcdefghijklmnopqrstuvwxyz'):
+def dechiper(message, type, key):
+    if(type == 'CAESAR'):
+        pass
+        
+def caesar_shift(message, shift, mode = 'ENCRYPT', custom_alphabet = 'abcdefghijklmnopqrstuvwxyz'):
     #key *= -1
     final_message = ''
-    default_alphabet = 'abcdefghijklmnopqrstuvwyz'
+    default_alphabet = 'abcdefghijklmnopqrstuvwxyz'
     for char in message:
         if char.isalpha():
             if(char.islower()):
-                final_message += custom_alphabet[(default_alphabet.index(char) + key) % len(custom_alphabet)]
+                final_message += custom_alphabet[(default_alphabet.index(char) + shift) % len(custom_alphabet)]
             elif(char.isupper()):
-                final_message += custom_alphabet[(default_alphabet.index(char.lower()) + key) % len(custom_alphabet)].upper()
+                final_message += custom_alphabet[(default_alphabet.index(char.lower()) + shift) % len(custom_alphabet)].upper()
         else:
             final_message += char
     return final_message
+
+print(caesar_shift("attack from south", 1, custom_alphabet="abc"))
